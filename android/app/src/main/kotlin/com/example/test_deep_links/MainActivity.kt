506c33a0e68830d671d2d8066fa50a90f9fa5488
Rajesh Malviya <rajveer0malviya@gmail.com>
2025-11-11 22:45:20 +0530

    Add logging

--- android/app/src/main/kotlin/com/example/test_deep_links/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/test_deep_links/MainActivity.kt
@@ -1,5 +1,20 @@
 package com.example.test_deep_links
 
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+private const val TAG = "flutter: MainActivity"
+
+class MainActivity : FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    Log.i(TAG, "onCreate: intent = $intent")
+    super.onCreate(savedInstanceState)
+  }
+
+  override fun onNewIntent(intent: Intent) {
+    Log.i(TAG, "onNewIntent: intent = $intent")
+    super.onNewIntent(intent)
+  }
+}
